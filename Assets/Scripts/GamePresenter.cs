f21dec594f3caa4f843bd07c050daf11f14b702a
ManuT18 <manuturo2008@gmail.com>
2025-11-16 16:29:25 -0300

    Ajustes en MainCanvas

--- Assets/Scripts/GamePresenter.cs
+++ Assets/Scripts/GamePresenter.cs
@@ -1,9 +1,6 @@
 using UnityEngine;
 
-/// <summary>
-/// Basic Unity script for GamePresenter.
-/// Attach this to a GameObject to implement presentation/gameflow logic.
-/// </summary>
+
 public class GamePresenter 
 {
      private GameView view;
@@ -13,15 +10,11 @@ public class GamePresenter
           this.view = view;
      }
      
-     // Start is called before the first frame update
      private void Start()
      {
-          // Initialization that may rely on other objects
      }
 
-     // Update is called once per frame
      private void Update()
      {
-          // Per-frame logic goes here
      }
 }
